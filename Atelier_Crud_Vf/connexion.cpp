d7e392b1a170e9ff5ae2415ee410fda29f247788
soumaya <soumaya.argoubi@esprit.tn>
2020-11-12 18:59:22 +0100

    Commit1

---
+++ Atelier_Crud_Vf/connexion.cpp
@@ -0,0 +1,17 @@
+#include "connexion.h"
+
+Connexion::Connexion()
+{}
+
+bool Connexion::ouvrirConnexion()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("Source_Projet2A");
+db.setUserName("soumaya");//inserer nom de l'utilisateur
+db.setPassword("admin");//inserer mot de passe de cet utilisateur
+if (db.open())
+    test=true;
+return  test;
+}
+void Connexion::fermerConnexion()
+{db.close();}
